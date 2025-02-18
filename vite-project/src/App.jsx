c85e29e7c1e0bef658a93df04dc20a2e6c8e7fa8
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2025-02-18 18:46:56 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Food from "./food";
+import Footer from "./Footer";
+import Header from "./header"
+
+function App() {
+return (
+  <>
+<Header/>
+<Food/>
+<Footer/>
+
+</>
+);
+}
+
+export default App
